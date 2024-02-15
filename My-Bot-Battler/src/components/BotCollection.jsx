9aba15df12ab5e13832e0644a1ebdbfdf266b959
abdikadharjama <abdikadharjama@gmail.com>
2024-02-15 18:36:35 +0300

    modified git commit -m modified
    git commit -m modified

--- My-Bot-Battler/src/components/BotCollection.jsx
+++ My-Bot-Battler/src/components/BotCollection.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import { Box, Heading, Image, Button } from '@chakra-ui/react';
 
 function BotCollection({ bots, enlistBot }) {
+  const handleEnlistBot = (bot) => {
+    enlistBot(bot);
+  };
+
   return (
-    <div className="bot-collection">
-      {bots.map((bot) => (
-        <div key={bot.id} onClick={() => enlistBot(bot)}>
-          <img src={bot.avatar_url} alt={bot.name} />
-          <h2>{bot.name}</h2>
-          <p>{bot.catchphrase}</p>
-        </div>
-      ))}
-    </div>
+    <Box p="4" borderWidth="1px" borderRadius="lg">
+      <Heading as="h2" size="md" mb="4">Bot Collection</Heading>
+      <Box display="grid" gridTemplateColumns="repeat(auto-fill, minmax(200px, 1fr))" gap="4">
+        {bots.map((bot) => (
+          <Box key={bot.id} borderWidth="1px" borderRadius="md" overflow="hidden">
+            <Image src={bot.avatar_url} alt={bot.name} borderRadius="md" boxSize="200px" />
+            <Box p="2">
+              <Heading as="h3" size="sm" mt="2">{bot.name}</Heading>
+              <Button onClick={() => handleEnlistBot(bot)} colorScheme="teal" mt="2" size="sm">Enlist</Button>
+            </Box>
+          </Box>
+        ))}
+      </Box>
+    </Box>
   );
 }
 
--- My-Bot-Battler/src/components/YourBotArmy.jsx
+++ My-Bot-Battler/src/components/YourBotArmy.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
+import { Box, Heading, Image, Button, Flex } from '@chakra-ui/react';
 
 function YourBotArmy({ army, releaseBot, dischargeBot }) {
+  const handleReleaseBot = (botId) => {
+    releaseBot(botId);
+  };
+
+  const handleDischargeBot = (botId) => {
+    dischargeBot(botId);
+  };
+
   return (
-    <div className="your-bot-army">
-      <h2>Your Bot Army</h2>
-      {army.map((bot) => (
-        <div key={bot.id}>
-          <img src={bot.avatar_url} alt={bot.name} />
-          <h2>{bot.name}</h2>
-          <button onClick={() => releaseBot(bot.id)}>Release</button>
-          <button onClick={() => dischargeBot(bot.id)} style={{ marginLeft: '10px', color: 'red' }}>Discharge</button>
-        </div>
-      ))}
-    </div>
+    <Box p="4" borderWidth="1px" borderRadius="lg">
+      <Heading as="h2" size="md" mb="4">Your Bot Army</Heading>
+      <Flex flexWrap="wrap">
+        {army.map((bot) => (
+          <Box key={bot.id} borderWidth="1px" borderRadius="md" overflow="hidden" flex="0 0 200px" m="2">
+            <Image src={bot.avatar_url} alt={bot.name} borderRadius="md" boxSize="200px" />
+            <Box p="2">
+              <Heading as="h3" size="sm" mt="2">{bot.name}</Heading>
+              <Flex mt="2" justifyContent="space-between">
+                <Button onClick={() => handleReleaseBot(bot.id)} colorScheme="orange" size="sm">Release</Button>
+                <Button onClick={() => handleDischargeBot(bot.id)} colorScheme="red" size="sm">Discharge</Button>
+              </Flex>
+            </Box>
+          </Box>
+        ))}
+      </Flex>
+    </Box>
   );
 }
 
